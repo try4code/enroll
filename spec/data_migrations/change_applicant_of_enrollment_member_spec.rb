1c31caa903c5f4c26516a07e4c36cc7df61cfaee
Ji Wang <traceyw@vt.edu>
2017-10-05 09:08:44 -0400

    Ref 4740 fix rspec and finish rake task to change applicant id of an enrollment member

--- spec/data_migrations/change_applicant_of_enrollment_member_spec.rb
+++ spec/data_migrations/change_applicant_of_enrollment_member_spec.rb
@@ -12,41 +12,36 @@ describe ChangeApplicantOfEnrollmentMember, dbclean: :after_each do
     end
   end
 
-  describe "changing plan year's state" do
-    let(:family) { FactoryGirl.build(:family, :with_primary_family_member_and_dependent)}
-    let(:primary) { family.primary_family_member }
-    let(:dependents) { family.dependents }
-    let(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment, household: family.active_household)}
+  describe "change the applicant id of an enrollment member" do
+    let!(:family1) { FactoryGirl.create(:family, :with_primary_family_member)}
+    let!(:primary1) { family1.primary_applicant }
+    let!(:family2) { FactoryGirl.create(:family, :with_primary_family_member)}
+    let!(:primary2) { family2.primary_applicant }
+    let!(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment, household: family1.active_household)}
     let(:date) { DateTime.now - 10.days }
     let(:subscriber) { FactoryGirl.create(:hbx_enrollment_member, :hbx_enrollment => hbx_enrollment, eligibility_date: date, coverage_start_on: date, applicant_id: primary.id) }
-    let(:hbx_en_member1) { FactoryGirl.create(:hbx_enrollment_member,
+    let!(:hbx_en_member1) { FactoryGirl.create(:hbx_enrollment_member,
                                               :id => "111",
                                               :hbx_enrollment => hbx_enrollment,
                                               eligibility_date: date,
                                               coverage_start_on: date,
-                                              applicant_id: dependents.first.id) }
+                                              applicant_id: primary1.id) }
 
-    let(:new_member) { HbxEnrollmentMember.new({ :id => "222",
-                                                 :applicant_id => dependents.last.id,
-                                                 :eligibility_date => date,
-                                                 :coverage_start_on => date}) }
-
-
-      before :each do
-        hbx_enrollment.hbx_enrollment_members = [subscriber, hbx_en_member1]
-        hbx_enrollment.save!
-        allow(ENV).to receive(:[]).with("enrollment_hbx_id").and_return(hbx_enrollment.hbx_id)
-        allow(ENV).to receive(:[]).with("enrollment_member_id").and_return(hbx_en_member1.id)
-        allow(ENV).to receive(:[]).with("new_applicant_id").and_return(new_member.id)
-      end
-
-      it "change the applicant id of the hbx_enrollment_member" do
-        expect(hbx_en_member1.applicant_id).to eq dependents.first.id
-        subject.migrate
-        hbx_enrollment.reload!
-        hbx_en_member1.reload!
-        expect(hbx_en_member1.applicant_id).to eq new_member.id
-      end
+    before :each do
+     hbx_enrollment.hbx_enrollment_members = [hbx_en_member1]
+     hbx_enrollment.save!
+     allow(ENV).to receive(:[]).with("enrollment_hbx_id").and_return(hbx_enrollment.hbx_id)
+     allow(ENV).to receive(:[]).with("enrollment_member_id").and_return(hbx_en_member1.id)
+     allow(ENV).to receive(:[]).with("new_applicant_id").and_return(primary2.id)
+    end
+    it "change the applicant id of the hbx_enrollment_member" do
+      expect(hbx_en_member1.applicant_id).to eq primary1.id
+      subject.migrate
+      family1.reload
+      family2.reload
+      hbx_en_member1.reload
+      expect(hbx_en_member1.applicant_id).to eq primary2.id
+    end
   end
 end
 
