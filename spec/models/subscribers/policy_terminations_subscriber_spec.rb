d2c91723b5bc385d5ff084d64bf3afc19dbbff0a
Trey Evans <lewis.r.evans@gmail.com>
2017-12-18 13:38:27 -0500

    Refs #20215.  Negative examples.

--- spec/models/subscribers/policy_terminations_subscriber_spec.rb
+++ spec/models/subscribers/policy_terminations_subscriber_spec.rb
@@ -55,6 +55,30 @@ describe Subscribers::PolicyTerminationsSubscriber do
     end
   end
 
+  describe "given a termination event with qualify reason non_payment, for an already terminated enrollment" do
+    let(:termination_date) { Date.new(2017, 5, 30) }
+
+    let(:termination_payload) do
+      {
+        :resource_instance_uri => "urn:some_thing:policy#policy_id",
+        :event_effective_date => termination_date.strftime("%Y%m%d"),
+        :hbx_enrollment_ids => JSON.dump([enrollment_id]),
+        :qualifying_reason => "non_payment"
+      }
+    end
+
+    before :each do
+      allow(existing_enrollment).to receive(:may_terminate_coverage?).and_return(false)
+      allow(existing_enrollment).to receive(:may_terminate_for_non_payment?).and_return(false)
+    end
+
+    it "does nothing to the enrollment" do
+      expect(existing_enrollment).not_to receive(:terminate_for_non_payment!).with(termination_date)
+      expect(existing_enrollment).not_to receive(:terminate_coverage!).with(termination_date)
+      subject.call(termination_event_name, nil, nil, nil, termination_payload)
+    end
+  end
+
   describe "given a cancel event" do
     let(:termination_payload) do
       {
@@ -90,6 +114,27 @@ describe Subscribers::PolicyTerminationsSubscriber do
 
     it "cancels the enrollment" do
       expect(existing_enrollment).to receive(:cancel_for_non_payment!)
+      subject.call(cancelation_event_name, nil, nil, nil, termination_payload)
+    end
+  end
+
+  describe "given a cancel event with qualify reason non_payment, for an already canceled enrollment" do
+    let(:termination_payload) do
+      {
+        :resource_instance_uri => "urn:some_thing:policy#policy_id",
+        :hbx_enrollment_ids => JSON.dump([enrollment_id]),
+        :qualifying_reason => "non_payment"
+      }
+    end
+
+    before :each do
+      allow(existing_enrollment).to receive(:may_cancel_coverage?).and_return(false)
+      allow(existing_enrollment).to receive(:may_cancel_for_non_payment?).and_return(false)
+    end
+
+    it "does nothing to the enrollment" do
+      expect(existing_enrollment).not_to receive(:cancel_for_non_payment!)
+      expect(existing_enrollment).not_to receive(:cancel_coverage!)
       subject.call(cancelation_event_name, nil, nil, nil, termination_payload)
     end
   end
