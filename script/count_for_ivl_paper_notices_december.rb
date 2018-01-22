74f65bd1f41f8e8173b5dacf68dec211d04a249e
saikumar9 <saikumarkotagiri9@gmail.com>
2018-01-22 14:05:14 -0500

    Refs #21327 refactor to ignore case sensitive

--- script/count_for_ivl_paper_notices_december.rb
+++ script/count_for_ivl_paper_notices_december.rb
@@ -7,30 +7,31 @@ enr_notice_count = 0
 final_renewal_notice_count = 0
 first_reminder_notice_count = 0
 
-final_enr_subject = "Your Final Eligibility Results, Plan, And Option To Change Plans"
-enr_notice_subject = "Your Health or Dental Plan Enrollment and Payment Deadline"
-final_renewal_notice_subject = "Review Your Insurance Plan Enrollment and Pay Your Bill Now"
-first_reminder_notice_subject = "Reminder - You Must Submit Documents by the Deadline to Keep Your Insurance"
+final_enr_subject = ("Your Final Eligibility Results, Plan, And Option To Change Plans").downcase!
+enr_notice_subject = ("Your Health or Dental Plan Enrollment and Payment Deadline").downcase!
+final_renewal_notice_subject = ("Review Your Insurance Plan Enrollment and Pay Your Bill Now").downcase!
+first_reminder_notice_subject = ("Reminder - You Must Submit Documents by the Deadline to Keep Your Insurance").downcase!
 
 Person.all_consumer_roles.by_message_created_at_datetime_range(start_at, end_at).each do |person|
   begin
     if person.consumer_role.can_receive_paper_communication?
       messages = person.inbox.messages.by_created_at_date_range(start_at, end_at)
+      message_subjects = messages.map(&:subject).each { |subject1| subject1.downcase!}
 
-      if messages.map(&:subject).include?(final_enr_subject)
-        final_eligibility_notice_count += messages.by_created_at_date_range(start_at, end_at).where(subject: final_enr_subject).count
+      if message_subjects.include?(final_enr_subject)
+        final_eligibility_notice_count += messages.where(subject: final_enr_subject).count
       end
 
-      if messages.map(&:subject).include?(enr_notice_subject)
-        enr_notice_count += messages.by_created_at_date_range(start_at, end_at).where(subject: enr_notice_subject).count
+      if message_subjects.include?(enr_notice_subject)
+        enr_notice_count += messages.where(subject: enr_notice_subject).count
       end
 
-      if messages.map(&:subject).include?(final_renewal_notice_subject)
-        final_renewal_notice_count += messages.by_created_at_date_range(start_at, end_at).where(subject: final_renewal_notice_subject).count
+      if message_subjects.include?(final_renewal_notice_subject)
+        final_renewal_notice_count += messages.where(subject: final_renewal_notice_subject).count
       end
 
-      if messages.map(&:subject).include?(first_reminder_notice_subject)
-        first_reminder_notice_count += messages.by_created_at_date_range(start_at, end_at).where(subject: first_reminder_notice_subject).count
+      if message_subjects.include?(first_reminder_notice_subject)
+        first_reminder_notice_count += messages.where(subject: first_reminder_notice_subject).count
       end
     end
   rescue => e
